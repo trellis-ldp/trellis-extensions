22efbc5bc3e12bfae41e06536c498a0e659ce98f
Aaron Coburn <acoburn@apache.org>
2018-06-25 20:27:08 -0400

    Update migration script

--- deployment/src/rpm/postInstall.sh
+++ deployment/src/rpm/postInstall.sh
@@ -4,6 +4,7 @@
 /usr/bin/systemctl daemon-reload
 
 echo "In order to complete the setup of Trellis, please configure the database connection in /opt/trellis/etc/config.yml\n\n"
-echo "Once the configuration is correct, you can initialize the database with these commands:\n\n"
-echo "    /opt/trelils/bin/trellis-db db migrate /opt/trellis/etc/config.yml\n"
+echo "Once the configuration is correct, you can initialize the database with this command:\n\n"
+echo "    /opt/trelils/bin/migrate\n\n"
+echo "And you can start up the system with this command:\n\n"
 echo "    systemctl start trellis-db\n"
