d52941a3385cc288e4e8d9c6e5c6a5a68ea49c51
Aaron Coburn <acoburn@apache.org>
2018-08-17 17:03:36 -0400

    Use printf instead of echo

--- deployment/src/rpm/postInstall.sh
+++ deployment/src/rpm/postInstall.sh
@@ -3,8 +3,8 @@
 /usr/bin/systemctl link /opt/trellis/etc/trellis-db.service
 /usr/bin/systemctl daemon-reload
 
-echo "In order to complete the setup of Trellis, please configure the database connection in /opt/trellis/etc/config.yml\n\n"
-echo "Once the configuration is correct, you can initialize the database with this command:\n\n"
-echo "    /opt/trelils/bin/migrate\n\n"
-echo "And you can start up the system with this command:\n\n"
-echo "    systemctl start trellis-db\n"
+printf "In order to complete the setup of Trellis, please configure the database connection in /opt/trellis/etc/config.yml\n\n"
+printf "Once the configuration is correct, you can initialize the database with this command:\n\n"
+printf "    /opt/trelils/bin/migrate\n\n"
+printf "And you can start up the system with this command:\n\n"
+printf "    systemctl start trellis-db\n"
