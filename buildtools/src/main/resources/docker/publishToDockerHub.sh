1f69eaa2cc50de92a9145b07e8350c686cbf0f06
Aaron Coburn <acoburn@apache.org>
2019-11-12 11:02:50 -0500

    Adjust docker build script

--- buildtools/src/main/resources/docker/publishToDockerHub.sh
+++ buildtools/src/main/resources/docker/publishToDockerHub.sh
@@ -1,27 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 IMAGE=trellisldp/trellis-database
 
 VERSION=$(./gradlew -q getVersion)
 BRANCH=$(git branch 2>/dev/null | sed -n -e 's/^\* \(.*\)/\1/p')
 
-./gradlew assemble
 cd platform/quarkus
+../../gradlew assemble
 
-echo $VERSION
 TAG=latest
 # Use the develop tag for snapshots
-if [[ $VERSION == *SNAPSHOT ]]
-then
+if [[ $VERSION == *SNAPSHOT* ]]; then
     TAG=develop
 fi
 
 # Don't use latest/develop tags for maintenance branches
-if [[ $BRANCH == *.x ]]
-then
-    docker build -f src/main/docker/Dockerfile.jvm -t $IMAGE:$VERSION .
+if [[ $BRANCH == *.x ]]; then
+    docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$VERSION" .
 else
-    docker build -f src/main/docker/Dockerfile.jvm -t $IMAGE:$TAG -t $IMAGE:$VERSION .
+    docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$TAG" -t "$IMAGE:$VERSION" .
 fi
 
 docker push $IMAGE
