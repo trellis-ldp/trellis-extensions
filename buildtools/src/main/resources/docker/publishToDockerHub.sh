89c83d16787c2ada4961b30c96c1f36113273ada
Aaron Coburn <acoburn@apache.org>
2019-11-12 09:25:14 -0500

    Add docker build support for the quarkus system

---
+++ buildtools/src/main/resources/docker/publishToDockerHub.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+IMAGE=trellisldp/trellis-database
+
+VERSION=$(./gradlew -q getVersion)
+BRANCH=$(git branch 2>/dev/null | sed -n -e 's/^\* \(.*\)/\1/p')
+
+./gradlew assemble
+cd platform/quarkus
+
+TAG=latest
+# Use the develop tag for snapshots
+if [[ $VERSION == *SNAPSHOT* ]]
+then
+    TAG=develop
+fi
+
+# Don't use latest/develop tags for maintenance branches
+if [[ $BRANCH == *.x ]]
+then
+    docker build -f src/main/docker/Dockerfile.jvm -t $IMAGE:$VERSION .
+else
+    docker build -f src/main/docker/Dockerfile.jvm -t $IMAGE:$TAG -t $IMAGE:$VERSION .
+fi
+
+docker push $IMAGE
