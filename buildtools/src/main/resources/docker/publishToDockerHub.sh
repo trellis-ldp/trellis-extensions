e5d20bcd56db2ae524acfc374dd702c452d363bf
Aaron Coburn <acoburn@apache.org>
2019-11-15 10:38:22 -0500

    Build cloud-enabled docker image

--- buildtools/src/main/resources/docker/publishToDockerHub.sh
+++ buildtools/src/main/resources/docker/publishToDockerHub.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-IMAGE=trellisldp/trellis-database
+IMAGE=$1
+
+if [ -z $1 ]; then
+    echo "Please include an image name parameter (e.g. trellisldp/trellis-database)"
+    exit 1
+else
+    echo "Building docker image $IMAGE"
+fi
 
 VERSION=$(./gradlew -q getVersion)
 BRANCH=$(git branch 2>/dev/null | sed -n -e 's/^\* \(.*\)/\1/p')
 
 cd platform/quarkus
-../../gradlew assemble
 
 TAG=latest
 # Use the develop tag for snapshots
@@ -14,11 +20,17 @@ if [[ $VERSION == *SNAPSHOT* ]]; then
     TAG=develop
 fi
 
-# Don't use latest/develop tags for maintenance branches
-if [[ $BRANCH == *.x ]]; then
-    docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$VERSION" .
-else
-    docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$TAG" -t "$IMAGE:$VERSION" .
-fi
+if [[ -f "build/trellis-db-quarkus-${VERSION}-runner.jar" && -d "build/lib" ]]
+then
+    # Don't use latest/develop tags for maintenance branches
+    if [[ $BRANCH == *.x ]]; then
+        docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$VERSION" .
+    else
+        docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$TAG" -t "$IMAGE:$VERSION" .
+    fi
 
-docker push $IMAGE
+    docker push $IMAGE
+else
+    echo "Build artifacts not present. Please run 'gradle assemble' first"
+    exit 1
+fi
