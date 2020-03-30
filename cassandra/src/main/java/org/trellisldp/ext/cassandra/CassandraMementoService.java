7bdb28a8590f92c9e2e9e3969a5fb110a7bbe332
Aaron Coburn <acoburn@apache.org>
2020-03-30 17:08:53 -0400

    Add ability to disable memento handling

--- cassandra/src/main/java/org/trellisldp/ext/cassandra/CassandraMementoService.java
+++ cassandra/src/main/java/org/trellisldp/ext/cassandra/CassandraMementoService.java
@@ -14,9 +14,12 @@
 package org.trellisldp.ext.cassandra;
 
 import static java.time.temporal.ChronoUnit.SECONDS;
+import static java.util.Collections.emptySortedSet;
 import static java.util.concurrent.CompletableFuture.completedFuture;
 import static java.util.stream.Collectors.toCollection;
+import static org.eclipse.microprofile.config.ConfigProvider.getConfig;
 import static org.slf4j.LoggerFactory.getLogger;
+import static org.trellisldp.api.Resource.SpecialResources.MISSING_RESOURCE;
 
 import com.datastax.oss.driver.api.core.cql.AsyncResultSet;
 import com.datastax.oss.driver.api.core.uuid.Uuids;
@@ -41,12 +44,14 @@ import org.trellisldp.ext.cassandra.query.rdf.GetMemento;
 import org.trellisldp.ext.cassandra.query.rdf.Mementoize;
 import org.trellisldp.ext.cassandra.query.rdf.Mementos;
 
-
 /**
  * A {@link MementoService} that stores Mementos in a Cassandra table.
  */
 @ApplicationScoped
 public class CassandraMementoService implements MementoService, CassandraBuildingService {
+
+    /** The configuration key for enabling/disabling memento handling. */
+    public static final String CONFIG_CASSANDRA_VERSIONING = "trellis.cassandra.versioning";
 
     private static final Logger LOGGER = getLogger(CassandraMementoService.class);
 
@@ -57,6 +62,8 @@ public class CassandraMementoService implements MementoService, CassandraBuildin
     private final GetMemento getMemento;
 
     private final GetFirstMemento getFirstMemento;
+
+    private final boolean enabled;
 
     CassandraMementoService() {
         this(null, null, null, null);
@@ -69,40 +76,51 @@ public class CassandraMementoService implements MementoService, CassandraBuildin
         this.mementoize = mementoize;
         this.getMemento = getMemento;
         this.getFirstMemento = getFirstMemento;
+        this.enabled = getConfig().getOptionalValue(CONFIG_CASSANDRA_VERSIONING, Boolean.class)
+            .orElse(Boolean.TRUE);
     }
 
     @Override
     public CompletionStage<Void> put(final Resource r) {
 
-        final Metadata metadata = Metadata.builder(r).build();
-        final Dataset data = r.dataset();
-        final Instant modified = r.getModified();
-        final UUID creation = Uuids.timeBased();
+        if (enabled) {
+            final Metadata metadata = Metadata.builder(r).build();
+            final Dataset data = r.dataset();
+            final Instant modified = r.getModified();
+            final UUID creation = Uuids.timeBased();
 
-        LOGGER.debug("Writing Memento for {} at time: {}", metadata.getIdentifier(), modified);
-        return mementoize.execute(metadata, modified, data, creation);
+            LOGGER.debug("Writing Memento for {} at time: {}", metadata.getIdentifier(), modified);
+            return mementoize.execute(metadata, modified, data, creation);
+        }
+        return completedFuture(null);
     }
 
     //@formatter:off
     @Override
     public CompletionStage<SortedSet<Instant>> mementos(final IRI id) {
-        return mementos.execute(id)
-                        .thenApply(AsyncResultSetUtils::stream)
-                        .thenApply(results -> results
-                                        .map(row -> row.get("modified", Instant.class))
-                                        .map(time -> time.truncatedTo(SECONDS))
-                                        .collect(toCollection(TreeSet::new)));
+        if (enabled) {
+            return mementos.execute(id)
+                            .thenApply(AsyncResultSetUtils::stream)
+                            .thenApply(results -> results
+                                            .map(row -> row.get("modified", Instant.class))
+                                            .map(time -> time.truncatedTo(SECONDS))
+                                            .collect(toCollection(TreeSet::new)));
+        }
+        return completedFuture(emptySortedSet());
     }
     //@formatter:on
 
     @Override
     public CompletionStage<Resource> get(final IRI id, final Instant time) {
-        LOGGER.debug("Retrieving Memento for: {} at {}", id, time);
-        return getMemento.execute(id, time)
-                        .thenCompose(result -> result.remaining() > 0
-                                        ? getFirstMemento.execute(id)
-                                        : completedFuture(result))
-                        .thenApply(AsyncResultSet::one)
-                        .thenApply(row -> parse(row, LOGGER, id));
+        if (enabled) {
+            LOGGER.debug("Retrieving Memento for: {} at {}", id, time);
+            return getMemento.execute(id, time)
+                            .thenCompose(result -> result.remaining() > 0
+                                            ? getFirstMemento.execute(id)
+                                            : completedFuture(result))
+                            .thenApply(AsyncResultSet::one)
+                            .thenApply(row -> parse(row, LOGGER, id));
+        }
+        return completedFuture(MISSING_RESOURCE);
     }
 }
