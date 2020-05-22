d65fa06d3661a9f56292e955fd66ff36687d84f9
Aaron Coburn <acoburn@apache.org>
2020-05-22 12:43:03 -0400

    Remove database layer

--- buildtools/src/main/resources/docker/publishToDockerHub.sh
+++ buildtools/src/main/resources/docker/publishToDockerHub.sh
@@ -3,7 +3,7 @@
 IMAGE=$1
 
 if [ -z "$1" ]; then
-    echo "Please include an image name parameter (e.g. trellisldp/trellis-database)"
+    echo "Please include an image name parameter (e.g. trellisldp/trellis-cassandra)"
     exit 1
 else
     echo "Building docker image $IMAGE"
@@ -19,37 +19,22 @@ if [[ $VERSION == *SNAPSHOT* ]]; then
     TAG=develop
 fi
 
-if [[ $IMAGE == "trellisldp/trellis-ext-db" ]]; then
-    # Dropwizard-based image
-    cd platform/dropwizard
+# Quarkus-based images
+cd platform/quarkus
 
+if [[ -f "build/trellis-quarkus-${VERSION}-runner.jar" && -d "build/lib" ]]
+then
     # Don't use latest/develop tags for maintenance branches
     if [[ $BRANCH == *.x ]]; then
-        docker build -f src/main/docker/Dockerfile -t "$IMAGE:$VERSION" .
+        docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$VERSION" .
     else
-        docker build -f src/main/docker/Dockerfile -t "$IMAGE:$TAG" -t "$IMAGE:$VERSION" .
+        docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$TAG" -t "$IMAGE:$VERSION" .
     fi
 
     docker push "$IMAGE"
-
 else
-    # Quarkus-based images
-    cd platform/quarkus
-
-    if [[ -f "build/trellis-quarkus-${VERSION}-runner.jar" && -d "build/lib" ]]
-    then
-        # Don't use latest/develop tags for maintenance branches
-        if [[ $BRANCH == *.x ]]; then
-            docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$VERSION" .
-        else
-            docker build -f src/main/docker/Dockerfile.jvm -t "$IMAGE:$TAG" -t "$IMAGE:$VERSION" .
-        fi
-
-        docker push "$IMAGE"
-    else
-        echo "Build artifacts not present. Please run 'gradle assemble' first"
-        exit 1
-    fi
+    echo "Build artifacts not present. Please run 'gradle assemble' first"
+    exit 1
 fi
 
 
